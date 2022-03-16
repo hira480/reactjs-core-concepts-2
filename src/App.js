9681961beb661a8da530510f6123e2e94c3d7a46
hira480 <hiramannan8@gmail.com>
2022-03-16 11:50:41 +0600

    state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
-  const products = [
-    { name: 'Laptop', price: 53000 },
-    { name: 'Phone', price: 63000 },
-    { name: 'Watch', price: 5300 },
-    { name: 'Tablet', price: 23000 },
-  ]
   return (
     <div className="App">
-      {
-        products.map(product => <Product name={product.name} price={product.price}></Product>)
-      }
-      {/* <Product name="Laptop" price="47000"></Product>
-      <Product name="Phone" price="75000"></Product>
-      <Product name="Watch" price="3500"></Product> */}
+      <Counter></Counter>
     </div>
   );
 }
+
+function Counter() {
+  const [count, setCount] = useState(33)
+
+  const increaseCount = () => setCount(count + 1);
+  const decreaseCount = () => setCount(count - 1);
+
+
+  /* const increaseCount = () => {
+    const newCount = count + 1;
+    setCount(newCount);
+  } */
+
+  return (
+    <div>
+      <h1>Count: {count}</h1>
+      <button onClick={increaseCount}>Increase</button>
+      <button onClick={decreaseCount}>Decrease</button>
+    </div>
+  )
+}
+
+/* const products = [
+  { name: 'Laptop', price: 53000 },
+  { name: 'Phone', price: 63000 },
+  { name: 'Watch', price: 5300 },
+  { name: 'Tablet', price: 23000 },
+]  */
+
+// {
+//   products.map(product => <Product name={product.name} price={product.price}></Product>)
+// }
+// <Product name="Laptop" price="47000"></Product>
+// <Product name="Phone" price="75000"></Product>
+// <Product name="Watch" price="3500"></Product>
 
 function Product(props) {
   return (
